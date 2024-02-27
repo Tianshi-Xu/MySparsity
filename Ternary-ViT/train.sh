d610b00423a1d73456d7f8e39d6273f06573b2c2
1907FB <994876761@qq.com>
2024-02-27 22:17:02 +0800

    .

--- Ternary-ViT/train.sh
+++ Ternary-ViT/train.sh
@@ -12,7 +12,7 @@
 # CUDA_VISIBLE_DEVICES=5 python finetune_cirnas.py -c ./configs/datasets/cifar10_kd_nas_mbv2_finetune.yml /home/xts/code/dataset/cifar10 --model finetune_cifar_cir_nas_mobilenetv2
 
 # pretrain imagenet
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 python -m torch.distributed.launch --nproc_per_node=6 train_cirnas.py -c ./configs/datasets/imagenet_pretrain.yml /opt/dataset/imagenet --model pretrain_imagenet_cir_nas_mobilenetv2
-# CUDA_VISIBLE_DEVICES=0 python train_cirnas.py -c ./configs/datasets/imagenet_pretrain.yml /opt/dataset/imagenet --model pretrain_imagenet_cir_nas_mobilenetv2
+CUDA_VISIBLE_DEVICES=3,4,5,6 python -m torch.distributed.launch --nproc_per_node=4 train_cirnas.py -c ./configs/datasets/imagenet_pretrain.yml /opt/dataset/imagenet --model pretrain_imagenet_cir_nas_mobilenetv2
+# CUDA_VISIBLE_DEVICES=1 python train_cirnas.py -c ./configs/datasets/imagenet_pretrain.yml /opt/dataset/imagenet --model pretrain_imagenet_cir_nas_mobilenetv2
 
 # CUDA_VISIBLE_DEVICES=1 python train.py -c ./configs/quantized/resnet18_tiny/w2a2.yml /home/xts/code/dataset/tiny-imagenet-200/ --model tiny_resnet18
